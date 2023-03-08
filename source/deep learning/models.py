c10cf2cb6369e371a8b2b1e60f2a14149782e036
Flxmx <thomasfiou@hotmail.fr>
2023-03-08 17:30:04 +0100

    models in models.py data preparation as a function code cleaning

--- source/deep learning/models.py
+++ source/deep learning/models.py
@@ -3,12 +3,12 @@ import numpy as np
 import sys
 import os
 sys.path.append('../src/')
-import dataset_prep
+from tensorflow import keras
 
-def dense_network():
+def dense_network(input_sequence_shape):
     simple_model = keras.Sequential(
         [
-            layers.Dense(128, input_shape=(20, 13), activation="relu"),
+            layers.Dense(128, input_shape=(20, input_sequence_shape), activation="relu"),
             # layers.BatchNormalization(), # les batchNormalization fond baisser l'accuracy
             layers.Dense(256, activation="relu"),
             # layers.BatchNormalization(),
@@ -16,3 +16,111 @@ def dense_network():
             layers.Dense(1, activation="linear"),  # avec relu on perd un peu d'accuracy
         ]
     )
+    return simple_model
+
+def stacked_RNN(hidden_units=32, dense_units=1, input_sequence_shape=input_sequence_shape, activation=['relu', 'relu']):
+    '''
+    hidden_units : nombre de neurones dans la couche SimpleRNN
+    dense_units : nombre de neurones dans la couche Dense
+    activation : liste des deux fonctions d'activation
+    '''
+    
+    model = Sequential()
+    model.add(SimpleRNN(hidden_units, input_shape=(20,input_sequence_shape), return_sequences=True, activation=activation[0]))
+    model.add(SimpleRNN(32, activation=activation[0]))
+    model.add(keras.layers.BatchNormalization())
+    model.add(Dense(64, activation=activation[1]))
+    model.add(Dense(units=dense_units, activation='sigmoid'))
+    
+    return model
+
+
+def stacked_LSTM_small(input_sequence_shape):
+    lstm_model = keras.Sequential([
+    layers.Dense(32, input_shape=(20,input_sequence_shape), activation='relu'),
+    layers.LSTM(128, return_sequences=True),
+    layers.LSTM(128),
+    #layers.BatchNormalization(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+    ])
+    return lstm_model
+
+# Mix
+def mix_rnn_lstm(input_sequence_shape):
+    lstm_rnn_model = keras.Sequential([
+    layers.Dense(32,input_shape=(20,input_sequence_shape), activation='relu'),
+    layers.LSTM(128, return_sequences=True, activation='relu'), # return_sequences à True pour que la sortie soit de dimension 3
+    layers.BatchNormalization(),
+    layers.Dense(64, activation='relu'),
+    layers.SimpleRNN(64,activation='relu', return_sequences=True),
+    layers.SimpleRNN(32,activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(64, activation='relu'),
+    #layers.Dropout(0.5),
+    layers.Dense(1, activation='sigmoid')
+    ])
+    return lstm_rnn_model
+
+# Single GRU layer of 256 units
+def simple_GRU(input_sequence_shape):
+    simple_GRU = keras.models.Sequential()
+    simple_GRU.add( keras.layers.Dense(32,input_shape=(20,input_sequence_shape), activation='relu'))
+    simple_GRU.add( keras.layers.Dropout(0.2) )
+    simple_GRU.add( keras.layers.BatchNormalization() )
+    simple_GRU.add( keras.layers.GRU(256, return_sequences=False, activation='relu') )
+    simple_GRU.add( keras.layers.Dropout(0.2) )
+    simple_GRU.add( keras.layers.BatchNormalization() )
+    # simple_GRU.add( keras.layers.Dense(64, activation='relu') )
+    # simple_GRU.add( keras.layers.Dropout(0.2) )
+    simple_GRU.add( keras.layers.Dense(1, activation='sigmoid') )
+    return simple_GRU
+
+# 3-stacked GRU model
+def stacked_GRU(input_sequence_shape):
+    stacked_GRU_model = keras.models.Sequential()
+    stacked_GRU_model.add( keras.layers.Dense(32,input_shape=(20,input_sequence_shape), activation='relu'))
+    stacked_GRU_model.add( keras.layers.Dropout(0.2) )
+    stacked_GRU_model.add( keras.layers.BatchNormalization() )
+    stacked_GRU_model.add( keras.layers.GRU(256, return_sequences=True, activation='relu') )
+    stacked_GRU_model.add( keras.layers.GRU(256, return_sequences=True, activation='relu') )
+    stacked_GRU_model.add( keras.layers.GRU(256, return_sequences=False, activation='relu') )
+    stacked_GRU_model.add( keras.layers.Dropout(0.2) )
+    stacked_GRU_model.add( keras.layers.BatchNormalization() )
+    # stacked_GRU_model.add( keras.layers.Dense(64, activation='relu') )
+    # stacked_GRU_model.add( keras.layers.Dropout(0.2) )
+    stacked_GRU_model.add( keras.layers.Dense(1, activation='sigmoid') )
+    return stacked_GRU_model
+
+
+# 3-stacked LSTM model
+def stacked_LSTM(input_sequence_shape):
+    stacked_LSTM_model = keras.models.Sequential()
+    stacked_LSTM_model.add( keras.layers.Dense(32,input_shape=(20,input_sequence_shape), activation='relu'))
+    stacked_LSTM_model.add( keras.layers.Dropout(0.2) )
+    stacked_LSTM_model.add( keras.layers.BatchNormalization() )
+    stacked_LSTM_model.add( keras.layers.LSTM(256, return_sequences=True, activation='relu') )
+    stacked_LSTM_model.add( keras.layers.LSTM(256, return_sequences=True, activation='relu') )
+    stacked_LSTM_model.add( keras.layers.LSTM(256, return_sequences=False, activation='relu') )
+    stacked_LSTM_model.add( keras.layers.Dropout(0.2) )
+    stacked_LSTM_model.add( keras.layers.BatchNormalization() )
+    # stacked_LSTM_model.add( keras.layers.Dense(64, activation='relu') )
+    # stacked_LSTM_model.add( keras.layers.Dropout(0.2) )
+    stacked_LSTM_model.add( keras.layers.Dense(1, activation='sigmoid') )
+    return stacked_LSTM_model
+
+# 3-stacked RNN model
+def stacked_RNN(input_sequence_shape):
+    stacked_RNN_model = keras.models.Sequential()
+    stacked_RNN_model.add( keras.layers.Dense(32,input_shape=(20,input_sequence_shape), activation='relu'))
+    stacked_RNN_model.add( keras.layers.Dropout(0.2) )
+    stacked_RNN_model.add( keras.layers.BatchNormalization() )
+    stacked_RNN_model.add( keras.layers.SimpleRNN(256, return_sequences=True, activation='relu') )
+    stacked_RNN_model.add( keras.layers.SimpleRNN(256, return_sequences=True, activation='relu') )
+    stacked_RNN_model.add( keras.layers.SimpleRNN(256, return_sequences=False, activation='relu') )
+    stacked_RNN_model.add( keras.layers.Dropout(0.2) )
+    stacked_RNN_model.add( keras.layers.BatchNormalization() )
+    # stacked_RNN_model.add( keras.layers.Dense(64, activation='relu') )
+    # stacked_RNN_model.add( keras.layers.Dropout(0.2) )
+    stacked_RNN_model.add( keras.layers.Dense(1, activation='sigmoid') )
+    return stacked_RNN_model
--- source/src/dataset_prep.py
+++ source/src/dataset_prep.py
@@ -42,18 +42,71 @@ def import_dataset(
 def sample_dataset(dataset, sample_nb):
     return dataset.sample(sample_nb)
 
+def flat_sequence_creation(df):
+    senders_sequences = []
+    senders_label = []
+    senders = np.unique(df["sender"].values)
+    for sender in senders:
+        # Données d'un seul sender rangée en fonction de l'heure d'envoi
+        sender_data_sorted = df.loc[df['sender'] == sender].sort_values("sendTime")
 
-def data_preparation(dataset, test_size=0.1):
+        # On récupère la valeur du label pour ce sender
+        """ On remplasse toute les valeur !=0 en 1 """
+        if sender_data_sorted['label'].tolist()[0] != 0 :
+            label=1
+        else :
+            label = sender_data_sorted['label'].tolist()[0]
+        #On supprime les colonnes label et sender
+        sender_data_sorted = sender_data_sorted.drop(["label","sender"], axis=1)
+        
+        #sequence_array = []
+
+        length = sender_data_sorted.shape[0]
+        slide = 10
+        start = 0
+        end = 20
+
+        # On vérifie qu'il est possible de faire une séquence de taille 20
+        while length > 20:
+            # Extraction par tranche de 20 avec une inter de 10
+            sequence = sender_data_sorted[start:end]
+
+            # Labels correspondant
+            #labels =  pd.Series.tolist(sequence["label"])
+
+            # On transforme les 13 en 1, cette formule marche toujours si on met d'autres types d'attaques
+            #labels[:] = [x if x == 0 else 1 for x in labels]
+
+            # Attribution des tableaux numpy
+            senders_sequences.append(np.array(sequence.values.tolist(), dtype=np.float32))
+            senders_label.append(label)
+
+            # Mise à jour des variables
+            start += slide
+            end += slide
+            length -= 10
+        
+        print('Nombre de séquences : ',len(senders_sequences))
+    return senders_sequences, senders_label
+
+def data_preparation(df, sample=False, test_size=0.1):
+   
+    sorted_dataset = df.sort_values("sender")
+    sequence_test, label_test = flat_sequence_creation(sorted_dataset)
+
     # Transformation en array numpy
-    X = np.array(dataset.drop(["label"], axis=1))
-    y = np.array(dataset["label"])
+    X = np.array(sequence_test)
+    y = np.array(label_test, dtype=np.float32)
 
-    # TODO :  rajouter la ligne qui change tout ce qui n'est pas un 0 en 1
-    for i in range(len(y)):
-        if y[i] != 0:
-            y[i] = 1
-
+    # Réduire le temps de training en prenant les 100000 premiers éléments
+    if sample:
+        X = X[:100000]
+        y = y[:100000]
     # Séparation en données d'entrainement et de test
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
-
+    print("X_train : ", X_train.shape)
+    print("y_train : ", y_train.shape)
+    print("X_test : ", X_test.shape)
+    print("y_test : ", y_test.shape)
+    
     return X_train, X_test, y_train, y_test
